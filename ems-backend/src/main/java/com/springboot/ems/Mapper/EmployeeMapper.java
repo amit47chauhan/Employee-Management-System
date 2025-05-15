d62473ac52e18588128ccd5e2724f7714b1d9c64
amit47chauhan <chauhanamit.ak47@gmail.com>
2025-05-15 12:43:45 +0530

    introduction of EmployeeMapper that converts b/w Employee <--> EmployeeDto (that acts as our data model(Employee) and our API model(EmployeeDto). EmployeeDto is a Data Transfer Object that carries data b/w backend and frontend)

---
+++ ems-backend/src/main/java/com/springboot/ems/Mapper/EmployeeMapper.java
@@ -0,0 +1,41 @@
+package com.springboot.ems.Mapper;
+
+import com.springboot.ems.dto.EmployeeDto;
+import com.springboot.ems.entity.Employee;
+
+/*
+--------------Purpose-------------
+This mapping helps decouple our domain model (Employee) from what we expose to the outside world (EmployeeDto).
+This is a good practice because:
+-It prevents leaking internal structures.
+-we can control what data gets sent to the client.
+-DTOs can be customized for different use cases (e.g., hiding sensitive fields).
+ */
+
+//This declares the EmployeeMapper class, which provides mapping logic between entity and DTO.
+public class EmployeeMapper {
+
+    //Takes an Employee object as input, Returns an EmployeeDto object
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        //This creates and returns a new EmployeeDto by copying over selected fields from the Employee entity.
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    /*
+    When we're receiving data from a client (e.g., a frontend app or API call), that data typically comes in the form of a DTO, not an Entity.
+    To save it to the database, we need to convert it into an Entity — and that's where reverse mapping comes in.
+     */
+    public static  Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
---
+++ ems-backend/src/main/java/com/springboot/ems/dto/EmployeeDto.java
@@ -0,0 +1,20 @@
+package com.springboot.ems.dto;
+
+// will use tha EmployeeDto class to transfer the data b/w client and server
+// used as the response of the rest api
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
