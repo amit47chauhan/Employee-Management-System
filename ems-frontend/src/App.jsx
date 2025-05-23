a5474ea276d48f718cd849945f6ad42b760c2d8f
amit47chauhan <chauhanamit.ak47@gmail.com>
2025-05-23 11:04:18 +0530

    implementing routing methods in react

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,14 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import EmployeeList from "./components/EmployeeList";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 function App() {
   return (
-    <>
-      <Header />
-      <EmployeeList />
-      <Footer />
-    </>
+      <BrowserRouter>
+        <Header/>
+          <Routes>
+            <Route path="/" element={<EmployeeList/>}/>
+            <Route path="/employees" element={<EmployeeList/>}/>
+          </Routes>
+        <Footer />
+      </BrowserRouter>
   );
 }
 
