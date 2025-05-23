a4db979b2d3c0b743d90d88bb796082a90ab6c84
amit47chauhan <chauhanamit.ak47@gmail.com>
2025-05-23 12:45:10 +0530

    connected api to react to add employee object in table

--- ems-frontend/src/services/EmployeeService.js
+++ ems-frontend/src/services/EmployeeService.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const REST_API_BASE_URL = "http://localhost:8080/api/employees";
 
+//calls list of employees
 export const listEmployees = () => {
   return axios.get(REST_API_BASE_URL);
 };
+
+export const createEmployee = (employee) => axios.post(REST_API_BASE_URL, employee);
